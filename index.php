8f64264babc039a61c2ac7a5716d65dead430329
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-02 17:11:26 +0200

    First part

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php   
+
+$string1="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio reprehenderit blanditiis corporis nulla, quasi incidunt. Eius accusantium sit quam voluptatem, provident nisi vel iusto, ab repellat aliquam ex, pariatur tempore. nulla! ";
+
+$string2=$_GET["word"];
+
+$string3='***';
+
+
+
+
+
+
+
+?> 
+
+<p> <?php echo($string1) ?></p>
+
+<p> lunghezza paragrafo: <?php echo(strlen($string1) + strlen($string2))  ?></p>
+
