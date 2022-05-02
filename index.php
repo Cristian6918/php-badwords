50121b54f8349039885b4e83bc4c7130e3101998
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-02 18:27:38 +0200

    done

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
-<?php   
+<?php
 
-$string1="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio reprehenderit blanditiis corporis nulla, quasi incidunt. Eius accusantium sit quam voluptatem, provident nisi vel iusto, ab repellat aliquam ex, pariatur tempore. nulla! ";
+$string1 = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio reprehenderit blanditiis corporis nulla, quasi incidunt. Eius accusantium sit quam voluptatem, provident nisi vel iusto, ab repellat aliquam ex, pariatur tempore. nulla! ";
 
-$string2=$_GET["word"];
+$string2 = $_GET["word"];
 
-$string3='***';
+$string3 = '***';
 
-?> 
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <p> <?php echo($replaced=str_replace($string2, $string3, $string1))  ?></p>
-    <p> lunghezza  originale paragrafo: <?php echo(strlen($string1))  ?></p>
-    <p> lunghezza replaced paragrafo <?php echo(strlen($replaced))  ?></p>
-    
+    <p> <?php echo ($replaced = str_replace($string2, $string3, $string1))  ?></p>
+    <p> Orginal Length of the paragraph: <?php echo (strlen($string1))  ?></p>
+    <p> Lenght of the Replaced paragraph <?php echo (strlen($replaced))  ?></p>
+
+    <p> <?php  ?> </p>
+
 </body>
+
 </html>
-
-
-
